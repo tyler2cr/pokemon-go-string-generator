9b889bdafdddf5cced920116618e88767ce0eb64
tyler <tyler2cr@gmail.com>
2022-12-17 11:32:01 -0800

    angular universal schematic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import {HttpClientModule} from "@angular/common/http";
     BagStringComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
