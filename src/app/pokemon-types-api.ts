4a9b515087b879a5963e855a46133b8679c82258
tyler <tyler2cr@gmail.com>
2022-12-18 23:38:22 -0800

    Auto-generated commit [ci skip]

--- src/app/pokemon-types-api.ts
+++
@@ -1,8 +0,0 @@
-import {Type} from "./type";
-
-export interface PokemonTypesApi {
-  form: string,
-  pokemon_id: number,
-  pokemon_name: string,
-  type: Type[]
-}
--- src/app/resistance.ts
+++
@@ -1,6 +0,0 @@
-import {Type} from "./type";
-
-export interface Resistance {
-  type: Type,
-  modifier: number
-}
